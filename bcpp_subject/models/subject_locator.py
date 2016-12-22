3899b78793dec3d2cbdf36328cb2cd080fa832a7
Coulson <ckgathi@bhp.org.bw>
2016-12-22 08:55:45 +0200

    add subject identifier to locator

--- bcpp_subject/models/subject_locator.py
+++ bcpp_subject/models/subject_locator.py
@@ -7,9 +7,10 @@ from edc_base.bw.validators import BWCellNumber, BWTelephoneNumber
 from edc_base.model.models import HistoricalRecords, BaseUuidModel
 from edc_constants.choices import YES_NO_NA, YES, NO, NOT_APPLICABLE
 from edc_locator.model_mixins import LocatorModelMixin
+from edc_identifier.model_mixins import UniqueSubjectIdentifierFieldMixin
 
 
-class SubjectLocator(LocatorModelMixin, BaseUuidModel):
+class SubjectLocator(LocatorModelMixin, UniqueSubjectIdentifierFieldMixin, BaseUuidModel):
     """A model completed by the user to that captures participant locator information
     and permission to contact."""
 
