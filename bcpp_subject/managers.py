775f8560e3fa8d19ad7be73d59ae3542388df770
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2016-12-28 17:02:05 +0200

    added manager for subject consent

--- bcpp_subject/managers.py
+++ bcpp_subject/managers.py
@@ -13,3 +13,22 @@ class EnrollmentManager(models.Manager):
 
     def get_by_natural_key(self, subject_identifier_as_pk):
         return self.get(subject_identifier=subject_identifier_as_pk)
+
+
+class SubjectConsentManager(models.Manager):
+
+    def get_by_natural_key(
+            self, subject_identifier, version, internal_identifier, survey, household_identifier, plot_identifier):
+        return self.get(
+            subject_identifier=subject_identifier, version=version,
+            household_member__internal_identifier=internal_identifier,
+            household_member__household_structure__survey=survey,
+            household_member__household_structure__household_identifier=household_identifier,
+            household_member__household_structure__household__plot__plot_identifier=plot_identifier
+        )
+
+
+class CorrectConsentManager(models.Manager):
+
+    def get_by_natural_key(self, subject_identifier_as_pk):
+        return self.get(subject_identifier=subject_identifier_as_pk)
