074cb34ad04b595e2c060467cef6c98ba6daa761
Kagiso Motingwa <kagisomotingwa@gmail.com>
2016-12-30 18:10:18 +0200

    commented the unworking code

--- bcpp_subject/tests/test_circumcided.py
+++ bcpp_subject/tests/test_circumcided.py
@@ -1,35 +1,29 @@
-from django.test import TestCase
-from edc_visit_schedule.constants import YEARS
-from dateutil.relativedelta import relativedelta
-from datetime import date
-from models import Circumcised
-
-
-class CircumcisedModelTest(TestCase):
-    """Setup data with all required fields for circumcised"""
-    def setUp(self):
-        self.data = {
-            circ_date: date.today(),
-            when_circ: 14,
-            age_unit_circ: "14 years",
-            where_circ: "hospital",
-            where_circ_other:None,
-            why_circ:"medical-reason",
-            why_circ_other:None,
-
-
-    def test_if_circumcised == YES and not self.health_benefits_smc(self):
-        """Test to verify if the participant answered yes to circumcision and knows abt the benefits"""
-        self.data[' where_circ'] = "hospital"
-        self.assertFalse('if {}, what are the benefits of male circumcision?.')
-
-    def test_if_not_circumcised == NO and not self.health_benefits_smc(self):
-        """Test to verify if the participant answered yes to circumcision and knows abt the benefits"""
-        self.data[' where_circ_other'] = None
-        self.assertFalse()
-       
-#     def test_if_self.when_circ and not self.age_unit_circ:
-#         """Test If participant answered age of circumcision then you must provide time units"""
-#     def_if_not_circ and age_unit_circ:
-#             raise CircumcisionError(
-#                 'If you did not answer age of circumcision then you must not provide time units.')
+# from django.test import TestCase
+# from edc_visit_schedule.constants import YEARS
+# from dateutil.relativedelta import relativedelta
+# from datetime import date
+# from models import Circumcised
+# 
+# 
+# class CircumcisedModelTest(TestCase):
+#     """Setup data with all required fields for circumcised"""
+# #     def setUp(self):
+# #         self.data = {
+# #             circ_date: date.today(),
+# #             when_circ: 14,
+# #             age_unit_circ: "14 years",
+# #             where_circ: "hospital",
+# #             where_circ_other:None,
+# #             why_circ:"medical-reason",
+# #             why_circ_other:None,
+# 
+# 
+#     def test_if_circumcised == YES and not self.health_benefits_smc(self):
+#         """Test to verify if the participant answered yes to circumcision and knows abt the benefits"""
+#         self.data[' where_circ'] = "hospital"
+#         self.assertFalse('if {}, what are the benefits of male circumcision?.')
+# 
+#     def test_if_not_circumcised == NO and not self.health_benefits_smc(self):
+#         """Test to verify if the participant answered yes to circumcision and knows abt the benefits"""
+#         self.data[' where_circ_other'] = None
+#         self.assertFalse()
