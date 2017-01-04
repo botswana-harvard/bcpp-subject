7dd4309cf1b43e446980c9b50cba8bbbd224220f
erikvw <ew2789@gmail.com>
2017-01-04 21:11:18 +0200

    remove unneccessary app config overrides

--- bcpp_subject/apps.py
+++ bcpp_subject/apps.py
@@ -1,8 +1,5 @@
 from django.apps import AppConfig as DjangoApponfig
 
-from edc_lab.apps import AppConfig as EdcLabAppConfig
-from edc_visit_tracking.apps import AppConfig as EdcVisitTrackingAppConfigParent
-
 
 class AppConfig(DjangoApponfig):
     name = 'bcpp_subject'
@@ -10,13 +7,3 @@ class AppConfig(DjangoApponfig):
 
     def ready(self):
         from bcpp_subject.models.signals import subject_consent_on_post_save
-
-
-class EdcVisitTrackingAppConfig(EdcVisitTrackingAppConfigParent):
-    visit_models = {'bcpp_subject': ('subject_visit', 'bcpp_subject.subjectvisit'),
-                    'bcpp_lab': ('subject_visit', 'bcpp_subject.subjectvisit'), }
-
-
-class EdcLabAppConfig(EdcLabAppConfig):
-    app_label = 'bcpp_subject'
-    requisition = 'bcpp_lab.subjectrequisition'
