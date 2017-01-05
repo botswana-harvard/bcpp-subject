9978bc585f4a7386b76eae430544b2c6281cdeb4
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2017-01-05 19:17:52 +0200

    rule group , pima tests bhs

--- bcpp_subject/tests/test_baseline_survey_rulegroups.py
+++ bcpp_subject/tests/test_baseline_survey_rulegroups.py
@@ -16,8 +16,16 @@ class TestBaselineRuleSurveyRuleGroups(SubjectMixin, TestCase):
     def setUp(self):
         super().setUp()
         self.subject_visit_male = self.make_subject_visit_for_a_male_subject('T0')
+        self.subject_visit_female = self.make_subject_visit_for_consented_subject('T0')
         self.household_member = HouseholdMember.objects.filter(
             subject_identifier=self.subject_visit_male.subject_identifier)
+        self.subject_identifier = self.subject_visit_male.subject_identifier
+
+    def crf_metadata_obj(self, model, entry_status):
+        return CrfMetadata.objects.filter(
+            entry_status=entry_status,
+            model=model,
+            subject_identifier=self.subject_identifier)
 
     def test_hiv_car_adherence_and_pima1(self):
         """ HIV Positive took arv in the past but now defaulting, Should NOT offer POC CD4.
@@ -26,22 +34,16 @@ class TestBaselineRuleSurveyRuleGroups(SubjectMixin, TestCase):
             * HivCareAdherence
             * HivResult
         """
-        crf_metada_hivcareadherence = CrfMetadata.objects.filter(
-            entry_status=REQUIRED,
-            model='bcpp_subject.hivcareadherence',
-            subject_identifier=self.subject_visit_male.subject_identifier)
-        self.assertEqual(crf_metada_hivcareadherence.count(), 1)
+        self.subject_identifier = self.subject_visit_male.subject_identifier
 
-        crf_metada_pima = CrfMetadata.objects.filter(
-            entry_status=NOT_REQUIRED,
-            model='bcpp_subject.pima',
-            subject_identifier=self.subject_visit_male.subject_identifier
-        )
-        self.assertEqual(crf_metada_pima.count(), 1)
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.hivcareadherence', REQUIRED).count(), 1)
+
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.pima', NOT_REQUIRED).count(), 1)
 
         # add HivCarAdherence,
         mommy.make_recipe(
             'bcpp_subject.hivcareadherence',
+            first_positive=None,
             subject_visit=self.subject_visit_male,
             report_datetime=self.get_utcnow(),
             medical_care=NO,
@@ -51,4 +53,60 @@ class TestBaselineRuleSurveyRuleGroups(SubjectMixin, TestCase):
             arv_evidence=NO,  # this is the rule field
         )
         # said they have taken ARV so not required
-        self.assertEqual(crf_metada_pima.count(), 1)
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.pima', NOT_REQUIRED, ).count(), 1)
+
+    def test_hiv_car_adherence_and_pima2(self):
+        """If POS and on arv and have doc evidence, Pima not required, not a defaulter.
+
+        Models:
+            * HivCareAdherence
+            * HivResult
+        """
+        self.subject_identifier = self.subject_visit_female.subject_identifier
+
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.hivcareadherence', REQUIRED).count(), 1)
+
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.pima', NOT_REQUIRED).count(), 1)
+
+        # add HivCarAdherence,
+        mommy.make_recipe(
+            'bcpp_subject.hivcareadherence',
+            report_datetime=self.get_utcnow(),
+            subject_visit=self.subject_visit_female,
+            first_positive=None,
+            medical_care=NO,
+            ever_recommended_arv=NO,
+            ever_taken_arv=NO,
+            on_arv=YES,
+            arv_evidence=YES,
+        )
+        # on art so no need for CD4
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.pima', NOT_REQUIRED).count(), 1)
+
+    def test_hiv_car_adherence_and_pima3(self):
+        """If POS and on arv but do not have doc evidence, Pima required.
+
+        Models:
+            * HivCareAdherence
+            * HivResult
+        """
+
+        self.subject_identifier = self.subject_visit_male.subject_identifier
+
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.hivcareadherence', REQUIRED).count(), 1)
+
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.pima', NOT_REQUIRED).count(), 1)
+
+        # add HivCarAdherence,
+        mommy.make_recipe(
+            'bcpp_subject.hivcareadherence',
+            report_datetime=self.get_utcnow(),
+            subject_visit=self.subject_visit_male,
+            first_positive=None,
+            medical_care=NO,
+            ever_recommended_arv=NO,
+            ever_taken_arv=NO,
+            on_arv=YES,
+            arv_evidence=NO,
+        )
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.pima', NOT_REQUIRED).count(), 1)
