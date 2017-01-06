fbfba70844e7c5ba613ec7878f561dc7a3bd925d
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2017-01-06 18:05:44 +0200

    added more bhs rule groups tests

--- bcpp_subject/tests/test_baseline_survey_rulegroups.py
+++ bcpp_subject/tests/test_baseline_survey_rulegroups.py
@@ -4,12 +4,14 @@ from django.test import TestCase
 
 from edc_constants.constants import NO, YES, POS, NEG
 from edc_metadata.constants import REQUIRED, NOT_REQUIRED, KEYED
-from edc_metadata.models import CrfMetadata
+from edc_metadata.models import CrfMetadata, RequisitionMetadata
 
 from member.models.household_member import HouseholdMember
 
 from .test_mixins import SubjectMixin
 from datetime import timedelta
+from bcpp_subject.labs import elisa_panel, viral_load_panel, rdb_panel
+from bcpp_subject.constants import NOT_SURE
 
 
 class TestBaselineRuleSurveyRuleGroups(SubjectMixin, TestCase):
@@ -27,6 +29,13 @@ class TestBaselineRuleSurveyRuleGroups(SubjectMixin, TestCase):
             entry_status=entry_status,
             model=model,
             subject_identifier=self.subject_identifier)
+
+    def requisition_metadata_obj(self, model, entry_status, panel_name):
+        return RequisitionMetadata.objects.filter(
+            entry_status=entry_status,
+            model=model,
+            subject_identifier=self.subject_identifier,
+            panel_name=panel_name)
 
     def test_hiv_car_adherence_and_pima1(self):
         """ HIV Positive took arv in the past but now defaulting, Should NOT offer POC CD4.
@@ -265,3 +274,171 @@ class TestBaselineRuleSurveyRuleGroups(SubjectMixin, TestCase):
 
         self.assertEqual(self.crf_metadata_obj('bcpp_subject.hivuntested', NOT_REQUIRED).count(), 1)
         self.assertEqual(self.crf_metadata_obj('bcpp_subject.hivtested', REQUIRED).count(), 1)
+
+    def test_known_pos_on_art_no_doc_requires_cd4_only(self):
+        """If previous result is POS on art but no evidence, need to run CD4 (Pima).
+
+        See rule_groups.ReviewNotPositiveRuleGroup and
+        """
+        self.subject_identifier = self.subject_visit_male.subject_identifier
+
+        # add HivTestReview,
+        self.hivtest_review(POS)
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.hivcareadherence', REQUIRED).count(), 1)
+
+        # add HivCareAdherence,
+        hivcareadherence = mommy.make_recipe(
+            'bcpp_subject.hivcareadherence',
+            first_positive=None,
+            subject_visit=self.subject_visit_male,
+            report_datetime=self.get_utcnow(),
+            medical_care=NO,
+            ever_recommended_arv=NO,
+            ever_taken_arv=NO,
+            on_arv=YES,
+            arv_evidence=NO,
+        )
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.hivtestreview', KEYED).count(), 1)
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.hivcareadherence', KEYED).count(), 1)
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.hivresult', NOT_REQUIRED).count(), 1)
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.pima', NOT_REQUIRED).count(), 1)
+
+        hivcareadherence.on_arv = NO
+        hivcareadherence.save()
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.pima', REQUIRED).count(), 1)
+
+    def test_hiv_care_adherance_for_verbal_posetive_only(self):
+        """HivCareAdharance form should be made available any verbal positive,
+            not considering availability or lack thereof documentation.
+        """
+        self.subject_identifier = self.subject_visit_male.subject_identifier
+
+        mommy.make_recipe(
+            'bcpp_subject.hivtestinghistory',
+            report_datetime=self.get_utcnow(),
+            subject_visit=self.subject_visit_male,
+            has_tested=YES,
+            when_hiv_test='1 to 5 months ago',
+            has_record=NO,
+            other_record=NO)
+
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.hivcareadherence', REQUIRED).count(), 1)
+
+    def test_known_pos_on_art_with_doc_requires_cd4_only(self):
+        """If previous result is POS on art with doc evidence, do not run HIV or CD4.
+
+        See rule_groups.ReviewNotPositiveRuleGroup and
+        """
+        self.subject_identifier = self.subject_visit_male.subject_identifier
+        # add HivTestReview,
+        self.hivtest_review(POS)
+
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.hivcareadherence', REQUIRED).count(), 1)
+
+        # add HivCareAdherence,
+        mommy.make_recipe(
+            'bcpp_subject.hivcareadherence',
+            first_positive=None,
+            subject_visit=self.subject_visit_male,
+            report_datetime=self.get_utcnow(),
+            medical_care=NO,
+            ever_recommended_arv=NO,
+            ever_taken_arv=NO,
+            on_arv=YES,
+            arv_evidence=YES,  # this is the rule field
+        )
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.hivtestreview', KEYED).count(), 1)
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.hivcareadherence', KEYED).count(), 1)
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.hivresult', NOT_REQUIRED).count(), 1)
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.pima', NOT_REQUIRED).count(), 1)
+
+    def test_known_pos_no_art_but_has_doc_requires_cd4_only(self):
+        """If previous result is POS on art but no evidence, need to run CD4 (Pima).
+
+        This is a defaulter
+
+        See rule_groups.ReviewNotPositiveRuleGroup and
+        """
+        self.subject_identifier = self.subject_visit_male.subject_identifier
+
+        # add HivTestReview,
+        self.hivtest_review(POS)
+
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.hivcareadherence', KEYED).count(), 1)
+
+        # add HivCareAdherence,
+        mommy.make_recipe(
+            'bcpp_subject.hivcareadherence',
+            first_positive=None,
+            subject_visit=self.subject_visit_male,
+            report_datetime=self.get_utcnow(),
+            medical_care=NO,
+            ever_recommended_arv=NO,
+            ever_taken_arv=NO,
+            on_arv=NO,
+            arv_evidence=YES,  # this is the rule field
+        )
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.hivtestreview', KEYED).count(), 1)
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.hivcareadherence', KEYED).count(), 1)
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.hivresult', NOT_REQUIRED).count(), 1)
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.pima', NOT_REQUIRED).count(), 1)
+
+    def test_elisaresult_behaves_like_todayhivresult(self):
+        """when an elisa result is keyed in, a +ve result should result in RBD and VL
+            being REQUIRED just like Today's HivResult
+        """
+        self.subject_identifier = self.subject_visit_male.subject_identifier
+
+        # add HivTestReview,
+        self.hivtest_review(NEG)
+
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.hivresult', REQUIRED).count(), 1)
+
+        self.hiv_result('IND', self.subject_visit_male)
+
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.elisahivresult', REQUIRED).count(), 1)
+
+        mommy.make_recipe(
+            'bcpp_subject.subjectrequisition', subject_visit=self.subject_visit_male, report_datetime=self.get_utcnow(),
+            panel_name=elisa_panel, hiv_result=POS, hiv_result_datetime=self.get_utcnow()
+        )
+
+        mommy.make_recipe(
+            'bcpp_subject.elisahivresult', subject_visit=self.subject_visit_male, report_datetime=self.get_utcnow(),
+            panel_name=elisa_panel,
+        )
+        self.assertEqual(self.crf_metadata_obj('bcpp_subject.elisahivresult', KEYED).count(), 1)
+        self.assertEqual(self.requisition_metadata_obj('bcpp_subject.subjectrequisition', REQUIRED, viral_load_panel).count(), 1)
+        self.assertEqual(self.requisition_metadata_obj('bcpp_subject.subjectrequisition', REQUIRED, rdb_panel).count(), 1)
+
+    def test_normal_circumsition_in_y1(self):
+
+        self.subject_identifier = self.subject_visit_male.subject_identifier
+
+        def assert_circumsition(circumsition_entry_status, uncircumcised_entry_status, circumcised_entry_status):
+            self.assertEqual(self.crf_metadata_obj('bcpp_subject.circumsition', circumsition_entry_status).count(), 1)
+            self.assertEqual(self.crf_metadata_obj('bcpp_subject.uncircumcised', uncircumcised_entry_status).count(), 1)
+            self.assertEqual(self.crf_metadata_obj('bcpp_subject.circumcised', circumcised_entry_status).count(), 1)
+        assert_circumsition(REQUIRED, REQUIRED, REQUIRED)
+
+        circumcition = mommy.make_recipe(
+            'bcpp_subject.circumcision', subject_visit=self.subject_visit_male, report_datetime=self.get_utcnow(),
+            circumcised=YES
+        )
+        assert_circumsition(KEYED, NOT_REQUIRED, REQUIRED)
+
+        self.hiv_result(NEG, self.subject_visit_male)
+
+        # Enforce that entering an HivResult does not affect Circumcition Meta Data.
+        assert_circumsition(KEYED, NOT_REQUIRED, REQUIRED)
+
+        circumcition.circumcised = NO
+        circumcition.save()
+
+        assert_circumsition(KEYED, REQUIRED, NOT_REQUIRED)
+
+        circumcition.circumcised = NOT_SURE
+        circumcition.save()
+
+        assert_circumsition(KEYED, NOT_REQUIRED, NOT_REQUIRED)
+
