e53e99642f624c40fab146d76f52f7024afd6e84
KeletsoBotsalano <kbotsalano@bhp.org.bw>
2017-01-13 11:15:36 +0200

    adding default values for choice fields to avoid None in the  options

--- bcpp_subject/models/hypertension_cardiovascular.py
+++ bcpp_subject/models/hypertension_cardiovascular.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from edc_base.model.models import BaseUuidModel, HistoricalRecords
-from edc_constants.choices import YES_NO
+from edc_constants.choices import YES_NO, YES
 
 from ..choices import HEALTH_CARE_FACILITY, TOBACCO_SMOKING
 from .list_models import MedicationGiven, MedicationTaken
@@ -16,12 +16,14 @@ class HypertensionCardiovascular(BaseUuidModel):
         blood pressure and measure your waist and hips. Are you willing to have your blood \
         pressure and body measurements taken today?',
         choices=YES_NO,
-        max_length=5)
+        max_length=5,
+        default='Yes')
 
     hypertension_diagnosis = models.CharField(
         verbose_name='Have you ever been diagnosed with hypertension?',
         choices=YES_NO,
         null=True,
+        default='Yes',
         max_length=5)
 
     medications_taken = models.ManyToManyField(
@@ -46,59 +48,68 @@ class HypertensionCardiovascular(BaseUuidModel):
         verbose_name='If yes: Health facility providing care',
         choices=HEALTH_CARE_FACILITY,
         null=True,
-        max_length=50)
+        max_length=50,
+        default='clinic')
 
     salt_intake_counselling = models.CharField(
         verbose_name='Have you ever been counselled about salt intake by a health care worker in the past 3 years?',
         choices=YES_NO,
         null=True,
-        max_length=5)
+        max_length=5,
+        default='Yes')
 
     tobacco_smoking = models.CharField(
         verbose_name='Have you ever smoked tobacco products?',
         choices=TOBACCO_SMOKING,
         null=True,
-        max_length=10)
+        max_length=10,
+        default='never')
 
     tobacco_counselling = models.CharField(
         verbose_name='If yes to any prior smoking of tobacco products, have you been counselled about smoking \
         cessation / not taking up smoking by a healthcare worker in the past 3 years?',
         choices=YES_NO,
         null=True,
-        max_length=5)
+        max_length=5,
+        default=YES)
 
     weight_counselling = models.CharField(
         verbose_name='Have you ever been counselled about what weight you should aim for by a health care worker \
         in the past 3 years?',
         choices=YES_NO,
         null=True,
-        max_length=5)
+        max_length=5,
+        default='Yes')
 
     physical_activity_counselling = models.CharField(
         verbose_name='Have you ever been counselled about the amount of physical activity (or exercise) to \
         maintain by a healthcare worker in the past 3 years?',
         choices=YES_NO,
         null=True,
-        max_length=5)
+        max_length=5,
+        default='Yes')
 
     alcohol_counselling = models.CharField(
         verbose_name='Have you ever been counselled about correct alcohol intake by a healthcare worker in \
         the past 3 years?',
         choices=YES_NO,
         null=True,
-        max_length=5)
+        max_length=5,
+        default='Yes')
 
     blood_test_for_cholesterol = models.CharField(
         verbose_name='Have you ever had a blood test for high cholesterol in the past 3 years?',
         choices=YES_NO,
         null=True,
-        max_length=5)
+        max_length=5,
+        default='Yes')
 
     blood_test_for_diabetes = models.CharField(
         verbose_name='Have you ever had blood test for sugar diabetes in the past 3 years?',
         choices=YES_NO,
         null=True,
-        max_length=5)
+        max_length=5,
+        default='Yes')
 
     history = HistoricalRecords()
 
