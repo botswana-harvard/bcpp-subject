8700359345aca49b631e771858b054df0366b1e8
KeletsoBotsalano <kbotsalano@bhp.org.bw>
2017-01-13 12:53:08 +0200

    validation for blood pressure checking

--- bcpp_subject/forms/hypertension_cardiovascular_form.py
+++ bcpp_subject/forms/hypertension_cardiovascular_form.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from edc_base.model.constants import DEFAULT_BASE_FIELDS
+
 from ..choices import MEDICATIONS_TAKEN
 from ..models import HypertensionCardiovascular
 
@@ -19,6 +21,27 @@ class HypertensionCardiovascularForm(SubjectModelFormMixin):
         label='If yes: Are you still being given this \
         medication (respond for each one ticked)',
         widget=forms.CheckboxSelectMultiple())
+
+    def get_field_data_as_list(self):
+        field_list = []
+        for field in HypertensionCardiovascular._meta.fields:
+            if field.name not in ['may_take_blood_pressure'] + DEFAULT_BASE_FIELDS:
+                field_list.append(self.cleaned_data[field])
+            return field_list
+
+    def check_if_questions_are_answered(self):
+            field_data = self.get_field_data_as_list()
+            for _field_data in field_data:
+                if _field_data:
+                    return False
+            return True
+
+    def validate_may_take_blood_pressure(self):
+
+        if self.cleaned_data['may_take_blood_pressure'] == 'No':
+            if self.check_if_questions_are_answered() == True:
+                raise forms.ValidationError(
+                    'How many hours did it take you to get to the hospital?')
 
     class Meta:
 
