e6008abc6a8837dbfe685b2c0db1cad3b98ea03e
Itumeleng Mosweu <imosweu@nkange.bhp.org.bw>
2017-01-14 09:49:02 +0200

    subject_requisition_form tests

--- bcpp_subject/forms/subject_requisition_form.py
+++ bcpp_subject/forms/subject_requisition_form.py
@@ -3,6 +3,7 @@ from django import forms
 from ..models import SubjectRequisition
 
 from .form_mixins import SubjectModelFormMixin
+from ..constants import RESEARCH_BLOOD_DRAW, VIRAL_LOAD, MICROTUBE
 
 
 class SubjectRequisitionForm(SubjectModelFormMixin):
@@ -13,13 +14,13 @@ class SubjectRequisitionForm(SubjectModelFormMixin):
 
     def clean(self):
         cleaned_data = super().clean()
-        panel = cleaned_data.get('panel')
+        panel = cleaned_data.get('panel_name')
         if panel:
             estimated_volume = cleaned_data.get('estimated_volume')
-            if panel.name in ['Research Blood Draw', 'Viral Load']:
+            if panel in [RESEARCH_BLOOD_DRAW, VIRAL_LOAD]:
                 if (estimated_volume < 8.0 or estimated_volume > 10.0):
                     raise forms.ValidationError("The estimated volume should between 8.0 and 10.0 ml.")
-            elif panel.name == 'Microtube':
+            elif panel == MICROTUBE:
                 if (estimated_volume < 3.0 or estimated_volume > 5.0):
                     raise forms.ValidationError("The estimated volume should between 3.0 and 5.0 ml.")
         return cleaned_data
---
+++ bcpp_subject/tests/test_subject_requisition.py
@@ -0,0 +1,96 @@
+from django.test import TestCase
+
+from edc_constants.constants import YES
+
+from .test_mixins import SubjectMixin
+
+from ..constants import VIRAL_LOAD, MICROTUBE, RESEARCH_BLOOD_DRAW, ELISA
+from ..forms import SubjectRequisitionForm
+
+
+class TestSubjectRequisitionForm(SubjectMixin, TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.subject_visit = self.make_subject_visit_for_consented_subject('T0')
+
+        self.options = {
+            'report_datetime': self.get_utcnow(),
+            'subject_visit': self.subject_visit.id,
+            'panel_name': MICROTUBE,
+            'requisition_identifier': 'R1',
+            'requisition_datetime': self.get_utcnow(),
+            'drawn_datetime': None,
+            'is_drawn': YES,
+            'reason_not_drawn': None,
+            'specimen_identifier': None,
+            'study_site': 'Gaborone',
+            'specimen_type': 'xxx',
+            'item_type': 'tube',
+            'item_count': 1,
+            'estimated_volume': 4.0,
+            'comments': 'good',
+        }
+
+    def test_form_is_valid(self):
+        subject_requisition_form = SubjectRequisitionForm(data=self.options)
+        self.assertTrue(subject_requisition_form.is_valid())
+        self.assertTrue(subject_requisition_form.save())
+
+    def test_estimated_volume_RBD_less(self):
+        """Assert rbd estimated volume is below limit"""
+        self.options.update(panel_name=RESEARCH_BLOOD_DRAW)
+        subject_requisition_form = SubjectRequisitionForm(data=self.options)
+        self.assertFalse(subject_requisition_form.is_valid())
+
+        self.options.update(estimated_volume=9.0)
+        subject_requisition_form = SubjectRequisitionForm(data=self.options)
+        self.assertTrue(subject_requisition_form.save())
+
+    def test_estimated_volume_RBD_more(self):
+        """Assert rbd estimated volume is above limit"""
+        self.options.update(panel_name=RESEARCH_BLOOD_DRAW, estimated_volume=15.0)
+        subject_requisition_form = SubjectRequisitionForm(data=self.options)
+        self.assertFalse(subject_requisition_form.is_valid())
+
+    def test_estimated_volume_viral_load_less(self):
+        """Assert viral_load estimated volume is below limit"""
+        self.options.update(panel_name=VIRAL_LOAD)
+        subject_requisition_form = SubjectRequisitionForm(data=self.options)
+        self.assertFalse(subject_requisition_form.is_valid())
+
+        self.options.update(estimated_volume=9.0)
+        subject_requisition_form = SubjectRequisitionForm(data=self.options)
+        self.assertTrue(subject_requisition_form.save())
+
+    def test_estimated_volume_viral_load_more(self):
+        """Assert viral_load estimated volume is above limit"""
+        self.options.update(panel_name=VIRAL_LOAD, estimated_volume=15.0)
+        subject_requisition_form = SubjectRequisitionForm(data=self.options)
+        self.assertFalse(subject_requisition_form.is_valid())
+
+    def test_estimated_volume_microtube_less(self):
+        """Assert microtube estimated volume is below limit"""
+        self.options.update(panel_name=MICROTUBE, estimated_volume=2)
+        subject_requisition_form = SubjectRequisitionForm(data=self.options)
+        self.assertFalse(subject_requisition_form.is_valid())
+
+    def test_estimated_volume_microtube_more(self):
+        """Assert microtube estimated volume is above limit"""
+        self.options.update(panel_name=MICROTUBE, estimated_volume=8.0)
+        subject_requisition_form = SubjectRequisitionForm(data=self.options)
+        self.assertFalse(subject_requisition_form.is_valid())
+
+    def test_ELISA_panel_name_can_save(self):
+        """Assert ELISA panel form can save"""
+        self.options.update(panel_name=ELISA)
+        subject_requisition_form = SubjectRequisitionForm(data=self.options)
+        print(subject_requisition_form.errors)
+        self.assertTrue(subject_requisition_form.is_valid())
+        self.assertTrue(subject_requisition_form.save())
+
+    def test_VENOUS_panel_name_can_save(self):
+        """Assert VENOUS panel form can save"""
+        self.options.update(panel_name='Venous (HIV)')
+        subject_requisition_form = SubjectRequisitionForm(data=self.options)
+        self.assertTrue(subject_requisition_form.save())
