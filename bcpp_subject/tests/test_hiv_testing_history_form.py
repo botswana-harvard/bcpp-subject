33d40c5e5a06fbfe9bf3843865b602bf4f6ee21f
Itumeleng Mosweu <imosweu@nkange.bhp.org.bw>
2017-01-16 17:08:49 +0200

    test updates on hiv history and test review forms

--- bcpp_subject/tests/test_hiv_testing_history_form.py
+++ bcpp_subject/tests/test_hiv_testing_history_form.py
@@ -11,10 +11,14 @@ class TestHivTestingHistoryForm(SubjectMixin, TestCase):
 
     def setUp(self):
         super().setUp()
-        self.subject_visit = self.make_subject_visit_for_consented_subject('T0')
+        self.consent_data = {
+            'identity': '31721515',
+            'confirm_identity': '31721515',
+        }
+        self.bhs_subject_visit_female = self.make_subject_visit_for_consented_subject_female('T0', **self.consent_data)
         self.options = {
             'report_datetime': self.get_utcnow(),
-            'subject_visit': self.subject_visit.id,
+            'subject_visit': self.bhs_subject_visit_female.id,
             'has_tested': YES,
             'when_hiv_test': '1 to 5 months ago',
             'has_record': YES,
