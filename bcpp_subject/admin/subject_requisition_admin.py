2f476c00dd5ef9ce51789c39f448faf0a64f0d19
Itumeleng Mosweu <imosweu@nkange.bhp.org.bw>
2017-01-17 17:51:02 +0200

    radio fields for subject requisition

--- bcpp_subject/admin/subject_requisition_admin.py
+++ bcpp_subject/admin/subject_requisition_admin.py
@@ -10,4 +10,24 @@ from .modeladmin_mixins import CrfModelAdminMixin
 @admin.register(SubjectRequisition, site=bcpp_subject_admin)
 class SubjectRequisitionAdmin (CrfModelAdminMixin, admin.ModelAdmin):
 
+    fields = [
+            'subject_visit',
+            'panel_name',
+            'requisition_identifier',
+            'drawn_datetime',
+            'is_drawn',
+            'reason_not_drawn',
+            'specimen_identifier',
+            'study_site',
+            'specimen_type',
+            'item_type',
+            'item_count',
+            'estimated_volume',
+            'comments',
+        ]
     form = SubjectRequisitionForm
+
+    radio_fields = {
+            'is_drawn': admin.VERTICAL,
+            'item_type': admin.VERTICAL,
+        }
