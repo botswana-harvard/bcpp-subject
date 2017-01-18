ec88089e6ca338a61b28be2e80f1aa7d177ff393
Kagiso Motingwa <kagisomotingwa@gmail.com>
2017-01-18 16:21:03 +0200

    hospital admission test and form

--- bcpp_subject/tests/test_hospital_admission_form.py
+++ bcpp_subject/tests/test_hospital_admission_form.py
@@ -6,55 +6,28 @@ from .test_mixins import SubjectMixin
 
 from ..forms import HospitalAdmissionForm
 
-# from member.models.household_member import HouseholdMember
-# from member.models.household_member.HouseholdMember import household_structure
-
-from ..constants import T1
-
-from dateutil.relativedelta import relativedelta
-
 
 class TestHospitalAdmissionForm(SubjectMixin, TestCase):
 
     def setUp(self):
         super().setUp()
-
-        household_structure = self.make_household_ready_for_enumeration()
-        old_member = self.add_household_member(household_structure=household_structure)
-        self.add_enrollment_checklist(old_member)
-#         self.add_subject_consent(old_member)
-
         self.consent_data = {
-            'identity': '31722515',
-            'confirm_identity': '31722515',
+            'identity': '31721515',
+            'confirm_identity': '31721515',
             'report_datetime': self.get_utcnow(),
         }
-
-        report_datetime = self.get_utcnow() + relativedelta(years=1)
-        self.t1_visit = self.add_subject_visit_followup(old_member, T1, report_datetime)
-
-        self.subject_visit_t1 = self.make_subject_visit_for_consented_subject_male('T1', **self.consent_data)
-
-        next_household_structure = self.get_next_household_structure_ready(
-            old_member.household_structure, make_hoh=None)
-
-        self.t1_visit = self.add_subject_visit_followup(old_member.next_household_structure, T1, report_datetime)
-
+        self.bhs_subject_visit_male = self.make_subject_visit_for_consented_subject_male('E0', **self.consent_data)
         self.options = {
-           'subject_visit': self.subject_visit_t1.id,
-           '÷≥lreport_ datetime': self.get_utcnow()+ relativedelta(years=1),
-           'admission_nights': 2,
-           'reason_hospitalized': 'Pregnancy',
-           'facility_hospitalized': 'Clinic',
-           'nights_hospitalized':  2,
-           'healthcare_expense': 100.00,
-           'travel_hours': '1 to under 2 hours',
-           'total_expenses': 100.00,
-           'hospitalization_costs': YES,
-           'household_structure': self.household_structure.id,
-           'old_member': self.old_member.id,
-           'next_household_structure': self.next_household_structure.id,
-           't1_visit': self.t1_visit.id,
+            'subject_visit': self.bhs_subject_visit_male.id,
+            'report_datetime': self.get_utcnow(),
+            'admission_nights': 2,
+            'reason_hospitalized': 'Pregnancy',
+            'facility_hospitalized': 'Clinic',
+            'nights_hospitalized': 2,
+            'healthcare_expense': 100.00,
+            'travel_hours': '1 to under 2 hours',
+            'total_expenses': 100.00,
+            'hospitalization_costs': YES,
         }
 
     def test_form_is_valid(self):
@@ -79,7 +52,7 @@ class TestHospitalAdmissionForm(SubjectMixin, TestCase):
     def test_zero_admission_nights_with_hospitalization_costs(self):
         """Asserts zero admission nights must have
         none hospitalization_costs"""
-        self.options.update(admission_nights=0)
+        self.options.update(admission_nights=None)
         form = HospitalAdmissionForm(data=self.options)
         self.assertFalse(form.is_valid())
 
