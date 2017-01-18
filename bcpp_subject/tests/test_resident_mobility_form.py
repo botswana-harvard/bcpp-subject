ba21f4eb23b3237402f5c43366382407598d90c1
Kagiso Motingwa <kagisomotingwa@gmail.com>
2017-01-18 07:42:54 +0200

    test recent mobility,pep8

--- bcpp_subject/tests/test_resident_mobility_form.py
+++ bcpp_subject/tests/test_resident_mobility_form.py
@@ -11,22 +11,27 @@ class TestResidencyMobilityForm(SubjectMixin, TestCase):
 
     def setUp(self):
         super().setUp()
-        self.subject_visit = self.make_subject_visit_for_consented_subject(
-            'T0')
+        self.consent_data = {
+            'identity': '31721515',
+            'confirm_identity': '31721515',
+            'report_datetime': self.get_utcnow(),
+        }
+        self.bhs_subject_visit_male = self.make_subject_visit_for_consented_subject_male('T0', **self.consent_data)
 
         self.options = {
-           'permanent_resident': NO,
-           'nights_away': 'more than 6 months',
-           'subject_visit': self.subject_visit.id,
-           'report_datetime': self.get_utcnow(),
-           'length_residence': '6 months to 12 months',
-           'intend_residency': YES,
-           'cattle_postlands': 'Farm/lands',
-           'cattle_postlands_other': 'Not Applicable', }
+            'permanent_resident': NO,
+            'nights_away': 'more than 6 months',
+            'subject_visit': self.bhs_subject_visit_male.id,
+            'report_datetime': self.get_utcnow(),
+            'length_residence': '6 months to 12 months',
+            'intend_residency': YES,
+            'cattle_postlands': 'Farm/lands',
+            'cattle_postlands_other': 'Not Applicable', }
 
     def test_form_is_valid(self):
         form = ResidencyMobilityForm(data=self.options)
         self.assertTrue(form.is_valid())
+        self.assertTrue(form.save())
 
     def test_perm_residency_nights_away(self):
         """Assert permanent resident cannot be more than 6months away
