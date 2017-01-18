22d4e402f752821db0a9d000b0c5eedd18f6ed85
erikvw <ew2789@gmail.com>
2017-01-18 06:11:58 +0200

    determine correct survey to consent to

--- bcpp_subject/views/dashboard/consent_view_mixin.py
+++ bcpp_subject/views/dashboard/consent_view_mixin.py
@@ -1,5 +1,7 @@
 from edc_dashboard.view_mixins import ConsentViewMixin as BaseConsentViewMixin
 
+from ...models.utils import get_enrollment_survey
+
 
 class ConsentViewMixin(BaseConsentViewMixin):
 
@@ -8,4 +10,7 @@ class ConsentViewMixin(BaseConsentViewMixin):
         return self.consent_model(
             subject_identifier=self.subject_identifier,
             household_member=self.household_member._original_object,
+            survey=get_enrollment_survey(
+                [obj for obj in self.consents if obj.id], self.survey_schedule_object),
+            survey_schedule=self.survey_schedule_object.field_value,
             version=self.consent_object.version)
