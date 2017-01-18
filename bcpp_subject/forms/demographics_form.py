319bc35920493f5747649646d98b41f7043f0028
Gofaone Magodi <gnmagodi@Djangos-MacBook-Pro-6.local>
2017-01-18 15:09:08 +0200

    fix demographics form for yellow pages

--- bcpp_subject/forms/demographics_form.py
+++ bcpp_subject/forms/demographics_form.py
@@ -43,14 +43,24 @@ class DemographicsForm(SubjectModelFormMixin):
             subject_identifier = subject_visit.subject_identifier
             consent = SubjectConsent.objects.filter(
                 household_member__subject_identifier=subject_identifier).last()
-            if num_wives <= 0:
+            if num_wives == 0:
                 if consent.gender == MALE:
                     raise forms.ValidationError(
                         {'num_wives': 'The number of wives should be greater than 0.'})
-            if husband_wives <= 0:
+            if husband_wives == 0:
                 if consent.gender == FEMALE:
                     raise forms.ValidationError(
                         {'num_wives': 'The number of husbands should be greater than 0.'})
+
+            if consent.gender == MALE:
+                if husband_wives is not None:
+                    raise forms.ValidationError(
+                        {'Male cannot have Husband'})
+
+            if consent.gender == FEMALE:
+                if num_wives is not None:
+                    raise forms.ValidationError(
+                        {'Male cannot have Husband'})
         return cleaned_data
 
     class Meta:
