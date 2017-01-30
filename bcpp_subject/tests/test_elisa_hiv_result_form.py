3b248ef5a4277ed8adcbfb9b9d4dd54aa77568ba
Itumeleng Mosweu <imosweu@nkange.bhp.org.bw>
2017-01-30 12:14:25 +0200

    elisa_hiv_result_form test updates

--- bcpp_subject/tests/test_elisa_hiv_result_form.py
+++ bcpp_subject/tests/test_elisa_hiv_result_form.py
@@ -12,21 +12,17 @@ class TestElisaHivResultForm(SubjectMixin, TestCase):
 
     def setUp(self):
         super().setUp()
-        self.consent_data = {
-            'identity': '31721515',
-            'confirm_identity': '31721515',
-            'report_datetime': self.get_utcnow(),
-        }
-        self.bhs_subject_visit_female = self.make_subject_visit_for_consented_subject_female('T0', **self.consent_data)
 
         mommy.make_recipe(
-            'bcpp_subject.subjectrequisition', subject_visit=self.bhs_subject_visit_female, report_datetime=self.get_utcnow(),
+            'bcpp_subject.subjectrequisition', 
+            subject_visit=self.subject_visit_female, 
+            report_datetime=self.get_utcnow(),
             panel_name='ELISA',
         )
 
         self.options = {
             'report_datetime': self.get_utcnow(),
-            'subject_visit': self.bhs_subject_visit_female.id,
+            'subject_visit': self.subject_visit_female.id,
             'hiv_result': NEG,
             'hiv_result_datetime': self.get_utcnow(),
         }
@@ -37,13 +33,13 @@ class TestElisaHivResultForm(SubjectMixin, TestCase):
         self.assertTrue(elisa_hiv_result_form.save())
 
     def test_no_date_time(self):
-        """Assert test with negative results was carried out without date provided"""
+        """Assert test with negative results was carried out without date provided."""
         self.options.update(hiv_result_datetime=None)
         elisa_hiv_result_form = ElisaHivResultForm(data=self.options)
         self.assertFalse(elisa_hiv_result_form.is_valid())
 
     def test_no_date_time_for_pos(self):
-        """Assert test with positive results was carried out without date provided"""
+        """Assert test with positive results was carried out without date provided."""
         self.options.update(hiv_result=POS, hiv_result_datetime=None)
         elisa_hiv_result_form = ElisaHivResultForm(data=self.options)
         self.assertFalse(elisa_hiv_result_form.is_valid())
