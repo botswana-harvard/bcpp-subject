a0bf04b63f92a5529fa608013527cb1590820ace
Kagiso Motingwa <kagisomotingwa@gmail.com>
2017-01-31 08:42:55 +0200

    updates to setup tests

--- bcpp_subject/tests/test_subject_requisition.py
+++ bcpp_subject/tests/test_subject_requisition.py
@@ -12,15 +12,9 @@ class TestSubjectRequisitionForm(SubjectMixin, TestCase):
 
     def setUp(self):
         super().setUp()
-        self.consent_data = {
-            'identity': '31721515',
-            'confirm_identity': '31721515',
-        }
-        self.bhs_subject_visit_female = self.make_subject_visit_for_consented_subject_female('E0', **self.consent_data)
-
         self.options = {
             'report_datetime': self.get_utcnow(),
-            'subject_visit': self.bhs_subject_visit_female.id,
+            'subject_visit': self.subject_visit_female.id,
             'panel_name': MICROTUBE,
             'requisition_identifier': 'R1',
             'requisition_datetime': self.get_utcnow(),
