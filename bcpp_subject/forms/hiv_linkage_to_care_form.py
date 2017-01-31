f847e3b7ed020eaf5b282e16cd8598cead8e01b3
erikvw <ew2789@gmail.com>
2017-01-31 21:01:44 +0200

    flake8

--- bcpp_subject/forms/hiv_linkage_to_care_form.py
+++ bcpp_subject/forms/hiv_linkage_to_care_form.py
@@ -3,7 +3,6 @@ from django import forms
 from edc_constants.constants import YES, NO
 
 from ..models import HivLinkageToCare, HivCareAdherence
-
 from .form_mixins import SubjectModelFormMixin
 
 
@@ -16,18 +15,25 @@ class HivLinkageToCareForm (SubjectModelFormMixin):
                 subject_visit=cleaned_data.get('subject_visit'))
         except HivCareAdherence.DoesNotExist:
             raise forms.ValidationError(
-                'Please complete {} first.'.format(HivCareAdherence._meta.verbose_name))
+                'Please complete {} first.'.format(
+                    HivCareAdherence._meta.verbose_name))
         else:
-            if hiv_care_adherence.on_arv == YES and cleaned_data.get('startered_therapy', None) == NO:
+            if (hiv_care_adherence.on_arv == YES
+                    and cleaned_data.get('startered_therapy') == NO):
                 raise forms.ValidationError(
-                    {'startered_therapy': 'If participant is said to be on ART on the {} '
-                        'this is contrary to the information given on question 12'.format(
-                            HivCareAdherence._meta.verbose_name)})
-            if hiv_care_adherence.on_arv == NO and cleaned_data.get('startered_therapy', None) == YES:
+                    {'startered_therapy':
+                     'If participant is said to be on ART on the {} '
+                     'this is contrary to the information given on '
+                     'question 12'.format(
+                         HivCareAdherence._meta.verbose_name)})
+            if (hiv_care_adherence.on_arv == NO
+                    and cleaned_data.get('startered_therapy') == YES):
                 raise forms.ValidationError(
-                    {'startered_therapy': 'If participant is said to be not on art on the '
-                        ' {} this is contrary to the information given on question 12'.format(
-                            HivCareAdherence._meta.verbose_name)})
+                    {'startered_therapy':
+                     'If participant is said to be not on art on the '
+                     ' {} this is contrary to the information given on '
+                     'question 12'.format(
+                         HivCareAdherence._meta.verbose_name)})
         return cleaned_data
 
     class Meta:
--- bcpp_subject/forms/hiv_result_form.py
+++ bcpp_subject/forms/hiv_result_form.py
@@ -14,7 +14,8 @@ class HivResultForm (SubjectModelFormMixin):
         cleaned_data = super(HivResultForm, self).clean()
 
         try:
-            HicEnrollment.objects.get(subject_visit=cleaned_data.get('subject_visit'))
+            HicEnrollment.objects.get(
+                subject_visit=cleaned_data.get('subject_visit'))
         except HicEnrollment.DoesNotExist:
             pass
         else:
@@ -32,48 +33,50 @@ class HivResultForm (SubjectModelFormMixin):
                 'Please complete Microtube Requisition first.')
 
         # validating when testing declined
-        if cleaned_data.get('hiv_result') == 'Declined' and not cleaned_data.get('why_not_tested'):
+        if (cleaned_data.get('hiv_result') == 'Declined'
+                and not cleaned_data.get('why_not_tested')):
             raise forms.ValidationError(
-                'If participant has declined testing, provide reason participant declined testing')
+                'If participant has declined testing, provide reason '
+                'participant declined testing')
 
         # validating when testing not performed
-        if cleaned_data.get('hiv_result') == 'Not performed' and cleaned_data.get('why_not_tested'):
+        if (cleaned_data.get('hiv_result') == 'Not performed'
+                and cleaned_data.get('why_not_tested')):
             raise forms.ValidationError(
                 {'why_not_tested': 'This field is not required.'})
 
         # testing declined but giving test date
         result_declined = (cleaned_data.get('hiv_result') == 'Declined')
-        result_not_performed = (cleaned_data.get('hiv_result') == 'Not performed')
-        if (result_declined or result_not_performed) and (cleaned_data.get('hiv_result_datetime')):
+        result_not_performed = (
+            cleaned_data.get('hiv_result') == 'Not performed')
+        if ((result_declined or result_not_performed)
+                and cleaned_data.get('hiv_result_datetime')):
             raise forms.ValidationError(
                 {'hiv_result_datetime': 'This field is not required.'})
 
         # testing done but giving reason why not done
-        if(
-           (cleaned_data.get('hiv_result') == POS) or
-           (cleaned_data.get('hiv_result') == NEG) or
-           (cleaned_data.get('hiv_result') == IND)) and (cleaned_data.get('why_not_tested')):
+        if (cleaned_data.get('hiv_result') in [POS, NEG, IND]
+                and cleaned_data.get('why_not_tested')):
             raise forms.ValidationError(
                 {'why_not_tested': 'This field is not required.'})
 
         # testing done but not providing date
-        if(
-           (cleaned_data.get('hiv_result') == POS) or
-           (cleaned_data.get('hiv_result') == NEG) or
-           (cleaned_data.get('hiv_result') == IND)) and not (cleaned_data.get('hiv_result_datetime')):
+        if (cleaned_data.get('hiv_result') in [POS, NEG, IND]
+                and not cleaned_data.get('hiv_result_datetime')):
             raise forms.ValidationError(
                 {'hiv_result_datetime': 'This field is required.'})
         self.validate_hiv_status_nd_blood_draw_type()
-        if(
-           cleaned_data.get('hiv_result') not in [POS, NEG, IND] and
-           cleaned_data.get('insufficient_vol') in [YES, NO]):
+        if (cleaned_data.get('hiv_result') not in [POS, NEG, IND]
+                and cleaned_data.get('insufficient_vol') in [YES, NO]):
             raise forms.ValidationError(
                 'No blood drawn.  You do not need to indicate if volume '
-                'is sufficient. Got {0}'.format(cleaned_data.get('insufficient_vol')))
+                'is sufficient. Got {0}'.format(
+                    cleaned_data.get('insufficient_vol')))
         if(
            cleaned_data.get('hiv_result') in [POS, NEG, IND] and
            cleaned_data.get('blood_draw_type') not in ['capillary', 'venous']):
-            raise forms.ValidationError('Blood was drawn. Please indicate the type.')
+            raise forms.ValidationError(
+                'Blood was drawn. Please indicate the type.')
         return cleaned_data
 
     def validate_hiv_status_nd_blood_draw_type(self):
@@ -87,11 +90,14 @@ class HivResultForm (SubjectModelFormMixin):
         if(
            cleaned_data.get('blood_draw_type') == 'capillary' and
            cleaned_data.get('insufficient_vol') == NOT_APPLICABLE):
-            raise forms.ValidationError('Please indicate if the capillary tube has sufficient volume.')
-        if cleaned_data.get('blood_draw_type') == 'venous' and cleaned_data.get('insufficient_vol') in [YES, NO]:
+            raise forms.ValidationError(
+                'Please indicate if the capillary tube has sufficient volume.')
+        if (cleaned_data.get('blood_draw_type') == 'venous'
+                and cleaned_data.get('insufficient_vol') in [YES, NO]):
             raise forms.ValidationError(
                 'Venous blood drawn.  You do not need to indicate if volume is '
-                'sufficient. Got {0}'.format(cleaned_data.get('insufficient_vol')))
+                'sufficient. Got {0}'.format(cleaned_data.get(
+                    'insufficient_vol')))
 
     class Meta:
         model = HivResult
