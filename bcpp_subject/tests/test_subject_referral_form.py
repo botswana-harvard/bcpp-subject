3360e2575eb1f7e595189beb354b8a2571c04f2c
Kagiso Motingwa <kagisomotingwa@gmail.com>
2017-01-31 08:30:36 +0200

    update setup

--- bcpp_subject/tests/test_subject_referral_form.py
+++ bcpp_subject/tests/test_subject_referral_form.py
@@ -15,43 +15,35 @@ class TestSubjectReferralForm(SubjectMixin, TestCase):
 
     def setUp(self):
         super().setUp()
-
-        self.consent_data = {
-            'identity': '31721515',
-            'confirm_identity': '31721515',
-        }
-        survey_schedule = self.get_survey_schedule(index=2)
-        self.bhs_subject_visit_female = self.make_subject_visit_for_consented_subject_female('E0', survey_schedule=survey_schedule, **self.consent_data)
-        report_datetime = self.bhs_subject_visit_female.report_datetime
+        report_datetime = self.subject_visit_female.report_datetime
         mommy.make_recipe(
             'bcpp_subject.subjectlocator',
-            subject_identifier=self.bhs_subject_visit_female.subject_identifier,
             report_datetime=report_datetime)
 
         mommy.make_recipe(
-            'bcpp_subject.subjectrequisition', subject_visit=self.bhs_subject_visit_female, report_datetime=report_datetime,
+            'bcpp_subject.subjectrequisition', subject_visit=self.subject_visit_female, report_datetime=report_datetime,
             panel_name=MICROTUBE,
         )
         mommy.make_recipe(
-            'bcpp_subject.hivresult', subject_visit=self.bhs_subject_visit_female, report_datetime=report_datetime,
+            'bcpp_subject.hivresult', subject_visit=self.subject_visit_female, report_datetime=report_datetime,
             hiv_result=POS, insufficient_vol=NO
         )
 
         mommy.make_recipe(
             'bcpp_subject.hivtestinghistory',
-            subject_visit=self.bhs_subject_visit_female,
+            subject_visit=self.subject_visit_female,
             report_datetime=report_datetime,
         )
 
         mommy.make_recipe(
             'bcpp_subject.hivtestreview',
             report_datetime=report_datetime,
-            subject_visit=self.bhs_subject_visit_female,
+            subject_visit=self.subject_visit_female,
             hiv_test_date=self.get_utcnow() - timedelta(days=50),
             recorded_hiv_result=POS)
 
         self.hivresultdocumentation = mommy.make_recipe(
-            'bcpp_subject.hivresultdocumentation', subject_visit=self.bhs_subject_visit_female,
+            'bcpp_subject.hivresultdocumentation', subject_visit=self.subject_visit_female,
             report_datetime=report_datetime
         )
         mommy.make_recipe(
@@ -63,7 +55,7 @@ class TestSubjectReferralForm(SubjectMixin, TestCase):
             'subject_visit': self.bhs_subject_visit_female.id,
             'referral_appt_comment': NOT_APPLICABLE,
             'subject_referred': YES,
-            'subject_identifier': self.bhs_subject_visit_female.subject_identifier,
+            'subject_identifier': self.subject_visit_female.subject_identifier,
             'created': self.get_utcnow(),
             'modified': self.get_utcnow(),
             'hostname_created': 'testuser',
