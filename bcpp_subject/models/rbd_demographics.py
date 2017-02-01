661cb285809ea1bfced9e44e4b4c84971ee06807
Kagiso Motingwa <kagisomotingwa@gmail.com>
2017-02-01 16:15:17 +0200

    removed double quote for strings

--- bcpp_subject/models/rbd_demographics.py
+++ bcpp_subject/models/rbd_demographics.py
@@ -21,7 +21,7 @@ class RbdDemographics (CrfModelMixin):
     ethnic = models.ManyToManyField(
         EthnicGroups,
         verbose_name="What is your ethnic group?",
-        help_text="Ask for the original ethnic group",
+        help_text='Ask for the original ethnic group',
     )
     ethnic_other = OtherCharField()
 
@@ -38,19 +38,19 @@ class RbdDemographics (CrfModelMixin):
                      " including yourself?",
         null=True,
         blank=True,
-        help_text="Leave blank if participant does not want to respond. (women only)",
+        help_text='Leave blank if participant does not want to respond. (women only)',
     )
     husband_wives = models.IntegerField(
         verbose_name="MEN: How many wives do you have, including traditional marriage?",
         null=True,
         blank=True,
-        help_text="Leave blank if participant does not want to respond. (men only)",
+        help_text='Leave blank if participant does not want to respond. (men only)',
     )
 
     live_with = models.ManyToManyField(
         LiveWith,
         verbose_name="Who do you currently live with ?",
-        help_text="[indicate all that apply]",
+        help_text='[indicate all that apply]',
     )
 
     history = HistoricalRecords()
