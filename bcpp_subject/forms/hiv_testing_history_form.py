37bc888092eb411aea0a11080f01b5e802375fd2
erikvw <ew2789@gmail.com>
2017-02-13 15:45:38 +0200

    use edc-base for validation

--- bcpp_subject/forms/hiv_testing_history_form.py
+++ bcpp_subject/forms/hiv_testing_history_form.py
@@ -16,8 +16,10 @@ class HivTestingHistoryForm (SubjectModelFormMixin):
             YES, field='has_tested', field_required='when_hiv_test')
         self.required_if(
             YES, field='has_tested', field_required='verbal_hiv_result')
-        self.applicable_if(
-            YES, field='has_tested', field_applicable='other_record')
+        self.applicable_if_true(
+            cleaned_data.get('has_tested') == YES and cleaned_data.get(
+                'verbal_hiv_result') == POS,
+            field_applicable='other_record')
         self.applicable_if(
             POS, field='verbal_hiv_result', field_applicable='other_record')
         return cleaned_data
