b2302aadb912331d67ffc8a3d3bc70dec3c236e3
erikvw <ew2789@gmail.com>
2017-02-14 18:22:01 +0200

    health care costs, fix q3

--- bcpp_subject/forms/hiv_health_care_costs_form.py
+++ bcpp_subject/forms/hiv_health_care_costs_form.py
@@ -1,4 +1,4 @@
-from edc_constants.constants import YES
+from edc_constants.constants import YES, NO
 
 from ..models import HivHealthCareCosts
 from .form_mixins import SubjectModelFormMixin
@@ -7,10 +7,10 @@ from .form_mixins import SubjectModelFormMixin
 class HivHealthCareCostsForm (SubjectModelFormMixin):
 
     def clean(self):
-        cleaned_data = super(HivHealthCareCostsForm, self).clean()
+        cleaned_data = super().clean()
 
         self.applicable_if(
-            YES, field='hiv_medical_care', field_applicable='reason_no_care')
+            NO, field='hiv_medical_care', field_applicable='reason_no_care')
         self.applicable_if(
             YES, field='hiv_medical_care', field_applicable='place_care_received')
         self.applicable_if(
