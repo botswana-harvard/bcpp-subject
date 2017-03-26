ca976db723f5712376fe002d832888760df76d4b
erikvw <ew2789@gmail.com>
2017-03-26 21:41:19 +0200

    fix toggling appt_status, prevent Done if subject locator not completed

--- bcpp_subject/forms/appointment_form.py
+++ bcpp_subject/forms/appointment_form.py
@@ -1,26 +1,22 @@
-import arrow
-
 from django import forms
 
 from edc_appointment.modelform_mixins import AppointmentFormMixin
-from edc_appointment.constants import NEW_APPT
-from edc_base.utils import get_utcnow
 
-from ..models import Appointment
+from ..models import Appointment, SubjectLocator
 
 
 class AppointmentForm(AppointmentFormMixin, forms.ModelForm):
 
-    def clean(self):
-        cleaned_data = super().clean()
-        if cleaned_data.get('appt_status') != NEW_APPT:
-            appt_datetime = cleaned_data.get('appt_datetime')
-            rappt_datetime = arrow.Arrow.fromdatetime(
-                appt_datetime, appt_datetime.tzinfo)
-            if rappt_datetime.to('UTC').date() > get_utcnow().date():
-                raise forms.ValidationError({
-                    'appt_datetime': 'Cannot be a future date.'})
-        return cleaned_data
+    @property
+    def required_additional_forms_exist(self):
+        """Returns True if any required additional forms are yet to be keyed.
+        """
+        try:
+            obj = SubjectLocator.objects.get(
+                subject_identifier=self.instance.subject_identifier)
+        except SubjectLocator.DoesNotExist:
+            obj = None
+        return False if obj else True
 
     class Meta:
         model = Appointment
