46eaa0bfcca30846c184ceb49d34e31b731254b2
erikvw <ew2789@gmail.com>
2017-08-03 09:41:05 +0200

    declare model names

--- bcpp_subject/forms/hic_enrollment_form.py
+++ bcpp_subject/forms/hic_enrollment_form.py
@@ -1,7 +1,16 @@
-from bcpp_subject_form_validators import HicEnrollmentFormValidator
+from bcpp_subject_form_validators import HicEnrollmentFormValidator as BaseValidator
 
 from ..models import HicEnrollment
 from .form_mixins import SubjectModelFormMixin
+
+
+class HicEnrollmentFormValidator(BaseValidator):
+
+    subject_consent_model = 'bcpp_subject.subjectconsent'
+    subject_locator_model = 'bcpp_subject.subjectlocator'
+    residency_mobility_model = 'bcpp_subject.residencymobility'
+    hiv_result_model = 'bcpp_subject.hivresult'
+    elisa_hiv_result_model = 'bcpp_subject.elisahivresult'
 
 
 class HicEnrollmentForm (SubjectModelFormMixin):
