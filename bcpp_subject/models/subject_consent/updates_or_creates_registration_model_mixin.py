7f2aa01dd61b012563d3afe7f40c9abd5c6d834c
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-09-01 12:02:15 +0200

    update or create registered subject based on internal identifier

--- bcpp_subject/models/subject_consent/updates_or_creates_registration_model_mixin.py
+++ bcpp_subject/models/subject_consent/updates_or_creates_registration_model_mixin.py
@@ -7,24 +7,27 @@ from edc_registration.model_mixins import UpdatesOrCreatesRegistrationModelMixin
 class UpdatesOrCreatesRegistrationModelMixin(Base):
 
     @property
-    def registration_options(self):
-        """Insert internal_identifier to be updated on
-        RegisteredSubject.
-        """
-        registration_options = super().registration_options
-        registration_options.update(
-            registration_identifier=self.household_member.internal_identifier.hex)
-        return registration_options
+    def registration_unique_field(self):
+        return 'internal_identifier'
+
+    @property
+    def registered_subject_unique_field(self):
+        return 'registration_identifier'
+
+#     @property
+#     def registration_options(self):
+#         """Insert internal_identifier to be updated on
+#         RegisteredSubject.
+#         """
+#         registration_options = super().registration_options
+#         registration_options.update(
+#             registration_identifier=self.household_member.internal_identifier.hex)
+#         return registration_options
 
     def registration_raise_on_illegal_value_change(self, registered_subject):
         """Raises an exception if a value changes between
         updates.
         """
-        if registered_subject.identity != self.identity:
-            raise RegisteredSubjectError(
-                'Identity may not be changed. Expected {}. Got {}'.format(
-                    registered_subject.identity,
-                    self.identity))
         if (registered_subject.registration_identifier
             and uuid.UUID(registered_subject.registration_identifier) !=
                 self.household_member.internal_identifier):
@@ -33,11 +36,6 @@ class UpdatesOrCreatesRegistrationModelMixin(Base):
                 'Got {}'.format(
                     registered_subject.registration_identifier,
                     self.household_member.internal_identifier))
-        if registered_subject.dob != self.dob:
-            raise RegisteredSubjectError(
-                'DoB may not be changed. Expected {}. Got {}'.format(
-                    registered_subject.dob,
-                    self.dob))
 
     class Meta:
         abstract = True
