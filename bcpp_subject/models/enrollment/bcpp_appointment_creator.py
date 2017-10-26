b860698aa629e8d9f4ed5c9288aa6b99cad37895
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-10-26 08:26:44 +0200

    fix but to create T2 appointment

--- bcpp_subject/models/enrollment/bcpp_appointment_creator.py
+++ bcpp_subject/models/enrollment/bcpp_appointment_creator.py
@@ -11,3 +11,16 @@ class BcppAppointmentCreator(AppointmentCreator):
             survey_schedule=self.model_obj.survey_schedule_object.field_value,
             household_member=self.model_obj.household_member)
         return options
+
+
+class BcppAhsAppointmentCreator(AppointmentCreator):
+
+    @property
+    def extra_create_appointment_options(self):
+        """User can add extra options for appointment.objects.create.
+        """
+        options = dict(
+            survey=self.model_obj.survey_object.field_value,
+            survey_schedule=self.model_obj.survey_schedule_object.field_value,
+            household_member=self.model_obj.get_household_member)
+        return options
