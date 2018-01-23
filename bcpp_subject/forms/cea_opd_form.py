7e7a893d13dd7b4adbb38f723b673b03a9283b15
Gofaone Magodi <magodign@bhp.org.bw>
2018-01-23 17:09:04 +0200

    pep8 fix

--- bcpp_subject/forms/cea_opd_form.py
+++ bcpp_subject/forms/cea_opd_form.py
@@ -1,9 +1,11 @@
 from ..models import CeaOpd
 from .form_mixins import SubjectModelFormMixin
+from bcpp_subject_form_validators.form_validators import CeaOpdFormValidator
 
 
 class CeaOpdForm (SubjectModelFormMixin):
-    pass
+
+    form_validator_cls = CeaOpdFormValidator
 
     class Meta:
         model = CeaOpd
