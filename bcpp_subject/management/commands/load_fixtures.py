bc478d9e4150f4aa2fbedfe82f500a0de13c2ed3
Coulson Thabo Kgathi <ckgathi@gmail.com>
2018-01-26 11:26:09 +0200

    load fixture

--- bcpp_subject/management/commands/load_fixtures.py
+++ bcpp_subject/management/commands/load_fixtures.py
@@ -5,5 +5,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         from bcpp_subject.list_data import list_data
+        from bcpp.load_data import preload_data
         from bcpp_subject.groups import groups
         print("Loaded {} crf lists".format(len(list_data)))
